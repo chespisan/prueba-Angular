93bb687b68059c95e52afd5064aabda8d41acbac
Sergio Sanchez <sergiosan2802@gmail.com>
2018-07-17 00:14:43 -0500

    challenge end

--- src/app/challenge/challenge.component.ts
+++ src/app/challenge/challenge.component.ts
@@ -18,6 +18,9 @@ export class ChallengeComponent implements OnInit {
     this.days = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
     // tslint:disable-next-line:max-line-length
     this.dates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 , 24, 25, 26, 27,28, 29, 30, 31, 1, 2, 3, 4];
+
+    this.minNum = 5;
+    this.maxNum = 15;
   }
 
   ngOnInit() {
